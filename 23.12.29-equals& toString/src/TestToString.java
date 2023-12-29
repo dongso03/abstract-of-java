f6f534eb0888a98380cef40d66c9349ef6bd2c50
GGG <GGG@DESKTOP-8N3GG4O>
2023-12-29 15:16:26 +0900

    d

--- 23.12.29-equals& toString/src/TestToString.java
+++ 23.12.29-equals& toString/src/TestToString.java
@@ -5,18 +5,22 @@ public class TestToString {
 		String str = "abcd";
 		Box b = new Box(100, 100, 100);
 		
-		System.out.println(o.toString());
-		System.out.println(str.toString());
-		System.out.println(b.toString());
-				
-		Person p = new Person("둘리", 55);
+		String concat = "문자열결합," + new Object();
+		System.out.println(concat);
+	
 		
-		System.out.println(p.toString());
-		
-		Customer c = new Customer("재욱", "010-22-222");
-		System.out.println(c.toString());
-		
-		Student s = new Student("동수", 95, 90, 85, new Pen(3000));
-		System.out.println(s.toString());
+//		System.out.println(o.toString());
+//		System.out.println(str.toString());
+//		System.out.println(b.toString());
+//				
+//		Person p = new Person("둘리", 55);
+//		
+//		System.out.println(p.toString());
+//		
+//		Customer c = new Customer("재욱", "010-22-222");
+//		System.out.println(c.toString());
+//		
+//		Student s = new Student("동수", 95, 90, 85, new Pen(3000));
+//		System.out.println(s.toString());
 	}
 }
